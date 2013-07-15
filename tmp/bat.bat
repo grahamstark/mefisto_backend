09a2374501d93a40137f8b2c25edba0b73b5ade7
Graham Stark <graham.stark@virtual-worlds.biz>
2013-07-15 16:16:14 +0100

    cleaned out tmp

--- tmp/bat.bat
+++
@@ -1,3 +0,0 @@
-"C:\Users\office\Documents\My files\Werk\EUROMOD\Model\__F4.15\Executable\EUROMOD.exe" "C:\Users\Office\Documents\My files\Werk\EUROMOD\Model\__F4.15\param\txt\EMConfig_be1.txt"
-"C:\Users\office\Documents\My files\Werk\EUROMOD\Model\__F4.15\Executable\EUROMOD.exe" "C:\Users\Office\Documents\My files\Werk\EUROMOD\Model\__F4.15\param\txt\EMConfig_be2.txt"
-"C:\Users\office\Documents\My files\Werk\EUROMOD\Model\__F4.15\Executable\EUROMOD.exe" "C:\Users\Office\Documents\My files\Werk\EUROMOD\Model\__F4.15\param\txt\EMConfig_be3.txt"
